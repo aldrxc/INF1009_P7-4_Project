6a3cc63e76cbc7d80dff1f2e6d6397980a05f780
aldricc <aldrictansl44@gmail.com>
2026-03-20 04:11:33 +0800

    added (commented out) sample method codes for loadOrganLayout() in CellDataManager.java and drawEntityHealthBar() in CellUIRenderer.java - full patch notes in tele

--- core/src/main/java/io/github/some_example_name/game/io/CellUIRenderer.java
+++ core/src/main/java/io/github/some_example_name/game/io/CellUIRenderer.java
@@ -16,9 +16,52 @@ public class CellUIRenderer {
     }
 
     public void drawEntityHealthBar(Entity entity) {
+        // TODO:
         // placeholder: can use ShapeRenderer or small Texture to draw a bar over
         // entity.getPosition()
     }
+
+    // THE COMMENTED OUT CODE BELOW IS A SAMPLE OF HOW THE ACTUAL
+    // drawEntityHealthBar() METHOD WILL BE LIKE:
+
+    // first - add this variable to top of CellUIRenderer class
+    // private com.badlogic.gdx.graphics.glutils.ShapeRenderer shapeRenderer = new
+    // com.badlogic.gdx.graphics.glutils.ShapeRenderer();
+
+    // // then - replace empty method with this
+    // public void drawEntityHealthBar(Entity entity) {
+    // // 1 - check if entity is a GameEntity - meaning it has health
+    // if (entity instanceof io.github.some_example_name.game.entity.GameEntity) {
+    // io.github.some_example_name.game.entity.GameEntity gameEntity =
+    // (io.github.some_example_name.game.entity.GameEntity) entity;
+
+    // // 2 - calculate health percentage - e.g. 50 / 100 = 0.5
+    // float healthPercent = gameEntity.getHp() / gameEntity.getMaxHp();
+
+    // // 3 - find entity's position on screen
+    // float x = gameEntity.getPositionX();
+    // // add offset - like 64 pixels - so bar floats above the cell
+    // float y = gameEntity.getPositionY() + 64;
+
+    // float barWidth = 50;
+    // float barHeight = 5;
+
+    // // 4 - draw shapes
+    // shapeRenderer.setProjectionMatrix(io.github.some_example_name.engine.io.IOManager.getInstance()
+    // .getOutputManager().getCamera().combined);
+    // shapeRenderer.begin(com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled);
+
+    // // draw red background - missing health
+    // shapeRenderer.setColor(com.badlogic.gdx.graphics.Color.RED);
+    // shapeRenderer.rect(x, y, barWidth, barHeight);
+
+    // // draw green foreground - current health - over red bar
+    // shapeRenderer.setColor(com.badlogic.gdx.graphics.Color.GREEN);
+    // shapeRenderer.rect(x, y, barWidth * healthPercent, barHeight);
+
+    // shapeRenderer.end();
+    // }
+    // }
 
     public void drawPlayerExpBar(int currentExp, int requiredExp) {
         OutputManager output = IOManager.getInstance().getOutputManager();
